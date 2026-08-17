d9d495a9895d8bad8954e5d1abc2f5b233d788b1
NeonGD1667 <neongmd1667@gmail.com>
2026-08-17 18:40:29 +0700

    Update gdr.cpp

--- src/gdr/gdr.cpp
+++ src/gdr/gdr.cpp
@@ -1,55 +1,102 @@
 #include <Geode/Geode.hpp>
 
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "gdr.hpp"
 
-cocos2d::CCPoint dataFromString(std::string dataString) {
-  std::stringstream ss(dataString);
-  std::string item;
-  std::vector<std::string> vec;
+cocos2d::CCPoint dataFromString(
+    std::string const& dataString
+) {
+    std::stringstream ss(dataString);
+    std::string item;
 
-  float xPos = 0.f;
-  float yPos = 0.f;
+    float xPos = 0.f;
+    float yPos = 0.f;
 
-  for (int i = 0; i < 3; i++) {
-    std::getline(ss, item, ',');
-    if (i == 1)
-      xPos = std::stof(item);
-    else if (i == 2)
-      yPos = std::stof(item);
-  }
+    for (int i = 0; i < 3; i++) {
+        if (!std::getline(ss, item, ','))
+            break;
 
-  return {xPos, yPos};
-};
+        if (i == 1) {
+            try {
+                xPos = std::stof(item);
+            } catch (...) {
+                xPos = 0.f;
+            }
+        } else if (i == 2) {
+            try {
+                yPos = std::stof(item);
+            } catch (...) {
+                yPos = 0.f;
+            }
+        }
+    }
 
-std::vector<std::string> splitByChar(std::string str, char splitChar) {
-  std::vector<std::string> strs;
-  strs.reserve(std::count(str.begin(), str.end(), splitChar) + 1);
-
-  size_t start = 0;
-  size_t end = str.find(splitChar);
-  while (end != std::string::npos) {
-    strs.emplace_back(str.substr(start, end - start));
-    start = end + 1;
-    end = str.find(splitChar, start);
-  }
-  strs.emplace_back(str.substr(start));
-
-  return strs;
+    return ccp(xPos, yPos);
 }
 
-geode::prelude::VersionInfo getVersion(std::vector<std::string> nums) {
-  if (nums.empty())
-    return geode::prelude::VersionInfo(0, 0, 0);
+std::vector<std::string> splitByChar(
+    std::string const& str,
+    char splitChar
+) {
+    std::vector<std::string> strs;
 
-  size_t major = geode::utils::numFromString<int>(nums[0]).unwrapOr(0);
-  size_t minor = nums.size() > 1
-                     ? geode::utils::numFromString<int>(nums[1]).unwrapOr(0)
-                     : 0;
-  size_t patch = nums.size() > 2
-                     ? geode::utils::numFromString<int>(nums[2]).unwrapOr(0)
-                     : 0;
+    strs.reserve(
+        std::count(
+            str.begin(),
+            str.end(),
+            splitChar
+        ) + 1
+    );
 
-  geode::prelude::VersionInfo ret(major, minor, patch);
+    size_t start = 0;
+    size_t end = str.find(splitChar);
 
-  return ret;
+    while (end != std::string::npos) {
+        strs.emplace_back(
+            str.substr(start, end - start)
+        );
+
+        start = end + 1;
+        end = str.find(splitChar, start);
+    }
+
+    strs.emplace_back(str.substr(start));
+
+    return strs;
+}
+
+geode::prelude::VersionInfo getVersion(
+    std::vector<std::string> const& nums
+) {
+    if (nums.empty())
+        return geode::prelude::VersionInfo(0, 0, 0);
+
+    size_t major =
+        geode::utils::numFromString<int>(
+            nums[0]
+        ).unwrapOr(0);
+
+    size_t minor =
+        nums.size() > 1
+            ? geode::utils::numFromString<int>(
+                  nums[1]
+              ).unwrapOr(0)
+            : 0;
+
+    size_t patch =
+        nums.size() > 2
+            ? geode::utils::numFromString<int>(
+                  nums[2]
+              ).unwrapOr(0)
+            : 0;
+
+    return geode::prelude::VersionInfo(
+        major,
+        minor,
+        patch
+    );
 }
