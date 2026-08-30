246859cfc903b53c6843dc25396f4f06b1d520a1
Neon <neongmd1667@gmail.com>
2026-08-30 10:43:12 +0700

    Update github-api.hpp

--- src/download/github-api.hpp
+++ src/download/github-api.hpp
@@ -1,30 +1,28 @@
 #pragma once
 
-#include <Geode/Geode.hpp>
 #include <Geode/utils/web.hpp>
 
 namespace github {
 
-struct Release {
-    std::string tagName;
-    std::string name;
-    std::string body;
-    bool prerelease = false;
-    bool draft = false;
-};
+inline void get(
+    std::string const& url,
+    std::function<void(geode::Result<geode::utils::web::WebResponse>)> callback
+) {
+    geode::async::spawn(
+        geode::utils::web::WebRequest()
+            .userAgent("DLL-Bot")
+            .get(url),
+        [callback](geode::utils::web::WebResponse response) {
+            if (!response.ok()) {
+                callback(geode::Err(
+                    fmt::format("HTTP request failed: {}", response.code())
+                ));
+                return;
+            }
 
-class API {
-public:
-    static void getLatestRelease(
-        std::string const& owner,
-        std::string const& repo,
-        std::function<void(geode::Result<Release>)> callback
+            callback(geode::Ok(std::move(response)));
+        }
     );
+}
 
-    static void download(
-        std::string const& url,
-        std::function<void(geode::Result<geode::utils::web::WebResponse>)> callback
-    );
-};
-
-} // namespace github
+}
