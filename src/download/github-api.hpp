91ddd12c58a433518b9f921c07093890c87c518a
Neon <neongmd1667@gmail.com>
2026-08-30 10:51:19 +0700

    Update github-api.hpp

--- src/download/github-api.hpp
+++ src/download/github-api.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
 #include <Geode/utils/web.hpp>
+#include <Geode/utils/async.hpp>
 
 namespace github {
 
+using WebResponse = geode::utils::web::WebResponse;
+
 inline void get(
     std::string const& url,
-    std::function<void(geode::Result<geode::utils::web::WebResponse>)> callback
+    geode::Function<void(geode::Result<WebResponse>)> callback
 ) {
     geode::async::spawn(
         geode::utils::web::WebRequest()
             .userAgent("DLL-Bot")
             .get(url),
-        [callback](geode::utils::web::WebResponse response) {
+        [callback = std::move(callback)](WebResponse response) mutable {
             if (!response.ok()) {
                 callback(geode::Err(
                     fmt::format("HTTP request failed: {}", response.code())
@@ -21,6 +24,27 @@ inline void get(
             }
 
             callback(geode::Ok(std::move(response)));
+        }
+    );
+}
+
+inline void download(
+    std::string const& url,
+    geode::Function<void(geode::Result<geode::ByteVector>)> callback
+) {
+    geode::async::spawn(
+        geode::utils::web::WebRequest()
+            .userAgent("DLL-Bot")
+            .get(url),
+        [callback = std::move(callback)](WebResponse response) mutable {
+            if (!response.ok()) {
+                callback(geode::Err(
+                    fmt::format("Download failed: HTTP {}", response.code())
+                ));
+                return;
+            }
+
+            callback(geode::Ok(response.data()));
         }
     );
 }
