ed1f4e9aab618ea77ee3cd91d109872aae5047d4
Neon <neongmd1667@gmail.com>
2026-09-04 16:31:11 +0700

    Update updater.hpp

--- src/download/updater.hpp
+++ src/download/updater.hpp
@@ -5,11 +5,5 @@ namespace updater {
 void check(bool notifyIfCurrent = false);
 void download();
 
-}#pragma once
-
-namespace updater {
-
-void check(bool notifyIfCurrent = false);
-void download();
-
 }
+
