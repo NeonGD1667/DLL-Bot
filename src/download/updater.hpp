f321a3708659f4713e79fd0f687c91a730a9f735
Neon <neongmd1667@gmail.com>
2026-09-04 16:30:45 +0700

    Create updater.hpp

---
+++ src/download/updater.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace updater {
+
+void check(bool notifyIfCurrent = false);
+void download();
+
+}#pragma once
+
+namespace updater {
+
+void check(bool notifyIfCurrent = false);
+void download();
+
+}
